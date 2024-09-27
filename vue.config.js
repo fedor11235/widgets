6927c15c5cd30443e7b2ea1495046829761a980f
Fedor <fedoravdeev3@gmail.com>
2024-09-28 01:48:19 +0700

    ref path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["quasar"],
 
+  publicPath: process.env.NODE_ENV === "production" ? "/widgets/" : "/",
+
   pluginOptions: {
     quasar: {
       importStrategy: "kebab",
